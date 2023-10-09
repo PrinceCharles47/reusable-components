11bf39e9e2707876c1e23b3f0c81768259636cdc
PrinceCharles47 <113660951+PrinceCharles47@users.noreply.github.com>
2023-10-09 23:23:08 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="wrapper">
+    <Navigation />
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Navigation,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+/* @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
-nav {
-  padding: 30px;
-}
+* {
+  font-family: "Poppins", sans-serif !important;
+  font-family: "Inter", sans-serif !important; */
+  /* font-family: "Roboto", sans-serif !important;
+} */
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+#wrapper {
+  background-color: #fafafa;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,164 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col md="9">
+        <Table :tableDetails="tableDetails" :filter="filter" />
+      </v-col>
+      <v-col md="3">
+        <p>{{ this.$route.params }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  name: "About",
+
+  components: {
+    Table,
+  },
+  data: () => ({
+    filter: {
+      isFilter: true,
+      columnsToFilter: [
+        {
+          columnValue: "specialization",
+          filteredArray: [],
+        },
+        {
+          columnValue: "hospital",
+          filteredArray: [],
+        },
+      ],
+    },
+
+    tableDetails: {
+      color: {
+        primary: "#7B40F9",
+        secondary: "#A981FF",
+      },
+
+      actions: [
+        {
+          name: "View",
+          color: "#7B40F9",
+        },
+      ],
+
+      headers: [
+        { text: "Doctor", value: "name", width: "25%" },
+        { text: "Specialization", value: "specialization", width: "25" },
+        { text: "Hospital/Clinic", value: "hospital", width: "35%" },
+        { text: "Action", value: "action", width: "15%" },
+      ],
+
+      items: [
+        {
+          name: "Dr. Sarah Johnson",
+          specialization: "Pediatrician",
+          hospital: "Sunshine Pediatrics Clinic",
+        },
+        {
+          name: "Dr. Emily Davis",
+          specialization: "Orthopedic Surgeon",
+          hospital: "Oakview Ortho Center",
+        },
+        {
+          name: "Dr. Michael Wilson",
+          specialization: "Dermatologist",
+          hospital: "Clear Skin Clinic",
+        },
+        {
+          name: "Dr. Linda Lee",
+          specialization: "Neurologist",
+          hospital: "NeuroCare Hospital",
+        },
+        {
+          name: "Dr. Robert Brown",
+          specialization: "Ophthalmologist",
+          hospital: "VisionCare Eye Center",
+        },
+        {
+          name: "Dr. Maria Garcia",
+          specialization: "Gynecologist",
+          hospital: "Women's Health Clinic",
+        },
+        {
+          name: "Dr. David Turner",
+          specialization: "ENT Specialist",
+          hospital: "Sound Ear and Throat Clinic",
+        },
+        {
+          name: "Dr. Jennifer White",
+          specialization: "Psychiatrist",
+          hospital: "Mental Wellness Center",
+        },
+        {
+          name: "Dr. James Miller",
+          specialization: "General Surgeon",
+          hospital: "City General Hospital",
+        },
+        {
+          name: "Dr. John Smith",
+          specialization: "Cardiologist",
+          hospital: "City Heart Hospital",
+        },
+        {
+          name: "Dr. Sarah Johnson",
+          specialization: "Pediatrician",
+          hospital: "Sunshine Pediatrics Clinic",
+        },
+        {
+          name: "Dr. Emily Davis",
+          specialization: "Orthopedic Surgeon",
+          hospital: "Oakview Ortho Center",
+        },
+        {
+          name: "Dr. Michael Wilson",
+          specialization: "Dermatologist",
+          hospital: "Clear Skin Clinic",
+        },
+        {
+          name: "Dr. Linda Lee",
+          specialization: "Neurologist",
+          hospital: "NeuroCare Hospital",
+        },
+        {
+          name: "Dr. Robert Brown",
+          specialization: "Ophthalmologist",
+          hospital: "VisionCare Eye Center",
+        },
+        {
+          name: "Dr. Maria Garcia",
+          specialization: "Gynecologist",
+          hospital: "Women's Health Clinic",
+        },
+        {
+          name: "Dr. David Turner",
+          specialization: "ENT Specialist",
+          hospital: "Sound Ear and Throat Clinic",
+        },
+        {
+          name: "Dr. Jennifer White",
+          specialization: "Psychiatrist",
+          hospital: "Mental Wellness Center",
+        },
+        {
+          name: "Dr. James Miller",
+          specialization: "General Surgeon",
+          hospital: "City General Hospital",
+        },
+      ],
+    },
+  }),
+};
+</script>
+
+<style scoped>
+/* * {
+  border: 1px solid gray;
+} */
+</style>
