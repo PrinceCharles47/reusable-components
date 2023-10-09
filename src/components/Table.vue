f879c9358be5da50e07261eab09e0b4af41a65f6
PrinceCharles47 <113660951+PrinceCharles47@users.noreply.github.com>
2023-10-09 23:23:43 +0800

    Table

---
+++ src/components/Table.vue
@@ -0,0 +1,300 @@
+<template>
+  <v-card flat class="px-4">
+    <v-card-title class="px-0">{{
+      tableDetails.title !== undefined ? tableDetails.title : null
+    }}</v-card-title>
+
+    <div class="d-flex align-center">
+
+      <v-text-field
+        hide-details="auto"
+        v-model="searchItem"
+        outlined
+        dense
+        placeholder="Search"
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
+    </div>
+
+    <v-row class="mt-4">
+      <v-dialog
+        v-model="dialogFilter"
+        transition="dialog-bottom-transition"
+        max-width="500"
+        persistent
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-if="filter !== undefined && filter.isFilter"
+            v-bind="attrs"
+            v-on="on"
+            elevation="0"
+            small
+            class="ml-3 mb-1"
+          >
+            Filters
+            <v-icon small class="ml-2" :color="tableDetails.color.primary"
+              >mdi-filter-variant</v-icon
+            >
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-toolbar
+            flat
+            :color="tableDetails.color.secondary"
+            class="white--text text-h6 px-4"
+            >FILTER ITEMS</v-toolbar
+          >
+
+          <v-form class="mx-8 mt-6">
+            <v-combobox
+              v-for="(option, i) in filterOptions"
+              :key="i"
+              v-model="option.filteredItems"
+              :items="option.items"
+              chips
+              clearable
+              :label="option.category.toUpperCase()"
+              multiple
+            >
+              <template v-slot:selection="{ attrs, item, select, selected }">
+                <v-chip
+                  v-bind="attrs"
+                  small
+                  :input-value="selected"
+                  close
+                  @click="select"
+                  @click:close="removeSelectedFilter(item)"
+                  outlined
+                  :text-color="tableDetails.color.primary"
+                  :color="tableDetails.color.secondary"
+                >
+                  {{ item }}
+                </v-chip>
+              </template>
+            </v-combobox>
+          </v-form>
+
+          <v-card-actions class="justify-end py-4 mx-4">
+            <v-btn text color="red" class="px-4" @click="dialogFilter = false"
+              >Close</v-btn
+            >
+            <v-btn
+              :color="tableDetails.color.primary"
+              class="white--text px-4"
+              @click="
+                dialogFilter = false;
+                filterList();
+              "
+              >Apply</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-col class="pt-0">
+        <v-chip
+          small
+          close
+          outlined
+          :text-color="tableDetails.color.primary"
+          :color="tableDetails.color.secondary"
+          class="mr-1 mb-1"
+          v-for="(chip, i) in chipNames"
+          :key="i"
+        >
+          {{ chip.toUpperCase() }}
+        </v-chip>
+      </v-col>
+    </v-row>
+
+    <v-data-table
+      :headers="tableDetails.headers"
+      :items="itemList"
+      :search="searchItem"
+      class="mt-4"
+      @click:row="redirect"
+    >
+      <template
+        v-if="tableDetails.actions !== undefined"
+        v-slot:[`item.action`]="{ item }"
+      >
+        <v-btn
+          small
+          elevation="0"
+          outlined
+          class="font-weight-bold mr-2"
+          v-for="(action, i) in tableDetails.actions"
+          :key="i"
+          :style="`border-color: #F0F0F0; color: ${action.color};`"
+        >
+          {{ action.name }}
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+//##################################################################
+// PROPS:
+// tableDetails: {
+//   colors: {
+//     primary: hex,
+//     secondary: hex
+//   }
+//   title: String (optional)
+//   withRedirect: {
+//     (row click event. use this or actions)
+//     to: String (route name)
+//   }
+//   actions: [
+//     (header with value: 'action' must be present in headers)
+//     {
+//       name: String,
+//       color: hex
+//     }
+//   ]
+//   items: Array of Objects
+//   headers: Array of Objects
+// }
+
+// filter: { (optional)
+//   isFilter: Boolean,
+//   columnsToFilter: [
+//     {
+//       columnValue: String, (value of column to filter in header)
+//       filteredArray: Empty Array, (will contain filtered items)
+//     },
+//   ],
+// },
+//##################################################################
+
+export default {
+  name: "Table",
+  props: {
+    tableDetails: Object,
+    filter: Object,
+  },
+  data: () => ({
+    searchItem: null,
+
+    dialogFilter: false,
+
+    itemList: [],
+    chipNames: [],
+
+    filterSelected: [],
+  }),
+
+  methods: {
+    //Resets the selected filter
+    removeSelectedFilter: function (item) {
+      this.filterSelected.splice(this.filterSelected.indexOf(item), 1);
+    },
+
+    filterList() {
+      //Returns filtered items else all table items
+
+      let newTableItems = [];
+      this.chipNames = []
+      let oldTableItems = this.tableDetails.items;
+      let currentFilter = null;
+      let previousFilter = null;
+
+      if (this.filter === undefined) {
+        return (this.itemList = this.tableDetails.items);
+      }
+
+      this.filterOptions.map((filter) => {
+        currentFilter = filter.category;
+        if (filter.filteredItems.length > 0) {
+          if (previousFilter !== currentFilter) {
+            previousFilter = currentFilter;
+            newTableItems = [];
+          }
+          filter.filteredItems.map((item) => {
+            this.getChipItems(item);
+            oldTableItems.map((tableItem) => {
+              if (
+                tableItem[filter.category].toLowerCase() === item.toLowerCase()
+              ) {
+                newTableItems.push(tableItem);
+                console.log(tableItem);
+              }
+            });
+          });
+
+          oldTableItems = newTableItems;
+        }
+
+        if (newTableItems.length <= 0) {
+          return (this.itemList = this.tableDetails.items);
+        }
+
+        console.log(newTableItems);
+        return (this.itemList = newTableItems);
+      });
+    },
+
+    getChipItems: function (item) {
+      this.chipNames.push(item);
+    },
+
+    redirect: function (row) {
+      if (this.tableDetails.withRedirect === undefined) {
+        return null;
+      }
+
+      this.$router.push({
+        name: this.tableDetails.withRedirect.to,
+        params: { ...row },
+      });
+    },
+  },
+
+  computed: {
+    tableItems() {
+      if (this.itemList.length === 0) {
+        return this.tableDetails.items;
+      }
+      return this.itemList;
+    },
+
+    // Returns the options or items to choose from when filtering table items
+    filterOptions() {
+      if (this.filter === undefined) {
+        return [];
+      }
+
+      let options = [];
+      let columnValues = [];
+
+      this.filter.columnsToFilter.forEach((column) => {
+        this.tableDetails.items.map((item) => {
+          columnValues.push(item[column.columnValue]);
+        });
+
+        let optionsSet = new Set(columnValues);
+
+        options.push({
+          category: column.columnValue,
+          filteredItems: column.filteredArray,
+          items: [...optionsSet],
+        });
+
+        columnValues = [];
+      });
+
+      return options;
+    },
+  },
+
+  mounted() {
+    this.itemList = this.tableDetails.items;
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,174 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <!-- <v-row>
+      <v-col md="9">
+        <Table :tableDetails="tableDetails" :filter="filter" />
+      </v-col>
+      <v-col md="3"></v-col>
+    </v-row> -->
+    <Table :tableDetails="tableDetails" :filter="filter" />
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Table from "../components/Table";
 
 export default {
-  name: 'HomeView',
+  name: "Home",
+
   components: {
-    HelloWorld
-  }
-}
+    Table,
+  },
+  data: () => ({
+    filter: {
+      isFilter: true,
+      columnsToFilter: [
+        {
+          columnValue: "specialization",
+          filteredArray: [],
+        },
+        {
+          columnValue: "hospital",
+          filteredArray: [],
+        },
+      ],
+    },
+
+    tableDetails: {
+      title: "Healthcare Providers",
+
+      color: {
+        primary: "#7B40F9",
+        secondary: "#A981FF",
+      },
+
+      withRedirect: {
+        to: "chart",
+      },
+
+      actions: [
+        {
+          name: "View",
+          color: "#7B40F9",
+        },
+      ],
+
+      headers: [
+        { text: "Doctor", value: "name", width: "25%" },
+        { text: "Specialization", value: "specialization", width: "25" },
+        { text: "Hospital/Clinic", value: "hospital", width: "35%" },
+        { text: "Action", value: "action", width: "15%" },
+      ],
+
+      items: [
+        {
+          name: "Dr. Sarah Johnson",
+          specialization: "Pediatrician",
+          hospital: "Sunshine Pediatrics Clinic",
+        },
+        {
+          name: "Dr. Emily Davis",
+          specialization: "Pediatrician",
+          hospital: "Oakview Ortho Center",
+        },
+        {
+          name: "Dr. Emily Davis",
+          specialization: "Orthopedic Surgeon",
+          hospital: "Oakview Ortho Center",
+        },
+        {
+          name: "Dr. Michael Wilson",
+          specialization: "Dermatologist",
+          hospital: "Clear Skin Clinic",
+        },
+        {
+          name: "Dr. Linda Lee",
+          specialization: "Neurologist",
+          hospital: "NeuroCare Hospital",
+        },
+        {
+          name: "Dr. Robert Brown",
+          specialization: "Ophthalmologist",
+          hospital: "VisionCare Eye Center",
+        },
+        {
+          name: "Dr. Maria Garcia",
+          specialization: "Gynecologist",
+          hospital: "Women's Health Clinic",
+        },
+        {
+          name: "Dr. David Turner",
+          specialization: "ENT Specialist",
+          hospital: "Sound Ear and Throat Clinic",
+        },
+        {
+          name: "Dr. Jennifer White",
+          specialization: "Psychiatrist",
+          hospital: "Mental Wellness Center",
+        },
+        {
+          name: "Dr. James Miller",
+          specialization: "General Surgeon",
+          hospital: "City General Hospital",
+        },
+        {
+          name: "Dr. John Smith",
+          specialization: "Cardiologist",
+          hospital: "City Heart Hospital",
+        },
+        {
+          name: "Dr. Sarah Johnson",
+          specialization: "Pediatrician",
+          hospital: "Sunshine Pediatrics Clinic",
+        },
+        {
+          name: "Dr. Emily Davis",
+          specialization: "Orthopedic Surgeon",
+          hospital: "Oakview Ortho Center",
+        },
+        {
+          name: "Dr. Michael Wilson",
+          specialization: "Dermatologist",
+          hospital: "Clear Skin Clinic",
+        },
+        {
+          name: "Dr. Linda Lee",
+          specialization: "Neurologist",
+          hospital: "NeuroCare Hospital",
+        },
+        {
+          name: "Dr. Robert Brown",
+          specialization: "Ophthalmologist",
+          hospital: "VisionCare Eye Center",
+        },
+        {
+          name: "Dr. Maria Garcia",
+          specialization: "Gynecologist",
+          hospital: "Women's Health Clinic",
+        },
+        {
+          name: "Dr. David Turner",
+          specialization: "ENT Specialist",
+          hospital: "Sound Ear and Throat Clinic",
+        },
+        {
+          name: "Dr. Jennifer White",
+          specialization: "Psychiatrist",
+          hospital: "Mental Wellness Center",
+        },
+        {
+          name: "Dr. James Miller",
+          specialization: "General Surgeon",
+          hospital: "City General Hospital",
+        },
+      ],
+    },
+  }),
+};
 </script>
+
+<style scoped>
+/* * {
+  border: 1px solid gray;
+} */
+</style>
